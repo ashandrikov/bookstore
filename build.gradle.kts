d8f05d5fb9ea11478bb4167fdb5fed55fd221828
Artemii <ashandrikov@gmail.com>
2026-03-04 18:31:20 +0300

    Add gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("org.postgresql:postgresql")
 }
 
 kotlin {
