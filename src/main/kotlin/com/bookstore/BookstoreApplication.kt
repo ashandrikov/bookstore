60c62bf682f050c9fba1f4865675496fa2569c22
Artemii <ashandrikov@gmail.com>
2026-03-16 19:05:20 +0300

    Add gitignore3

--- src/main/kotlin/com/bookstore/BookstoreApplication.kt
+++ src/main/kotlin/com/bookstore/BookstoreApplication.kt
@@ -1,11 +1,15 @@
 package com.bookstore
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class BookstoreApplication
 
+private val log = LoggerFactory.getLogger(BookstoreApplication::class.java)
+
 fun main(args: Array<String>) {
     runApplication<BookstoreApplication>(*args)
+    log.info("Bookstore application started successfully")
 }
